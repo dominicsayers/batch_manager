d6012cc3245d74630defbd669a66baa52b281168
Dominic Sayers <dominic@sayers.cc>
2018-09-27 17:22:54 +0100

    Configure RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+require 'simplecov'
+require 'simplecov-console'
+SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
+
+require 'batch_manager'
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.filter_run_when_matching :focus
+  config.order = :random
+  config.profile_examples = 10
+  config.run_all_when_everything_filtered = true
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.warnings = true
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  Kernel.srand config.seed
+end
