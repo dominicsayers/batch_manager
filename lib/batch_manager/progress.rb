d950f5bebfa24be49a2fd7eec039d24674a0b1ee
Dominic Sayers <dominic@sayers.cc>
2019-01-16 16:40:34 +0000

    Base progress on elapsed time not item count

--- lib/batch_manager/progress.rb
+++ lib/batch_manager/progress.rb
@@ -2,13 +2,14 @@ module BatchManager
   # Console output to show progress (if configured to do so)
   class Progress
     include ActionView::Helpers::NumberHelper
+    include ActionView::Helpers::DateHelper
 
-    attr_reader :state, :expected, :progress_each
+    attr_reader :state, :expected, :progress_every
 
     def initialize(job)
       @job = job
       self.expected = nil
-      self.progress_each = nil
+      self.progress_every = nil
       reset
     end
 
@@ -24,43 +25,42 @@ module BatchManager
 
     def show
       return unless show?
+      return unless compute_position
 
-      position = current_position
-      return unless (position % progress_each).zero?
-      return if position == previous_position
+      average_items_in_period = counter / stats[:frequency]
 
-      show_position(position, Time.current)
+      console "#{identifier}\t#{nice_number(counter)} items processed " \
+            "(" \
+            "rate: #{nice_number(items_in_period)} items in #{nice_elapsed_time(elapsed_time)}, " \
+            "average: #{nice_number(average_items_in_period)} items, " \
+            "max: #{nice_number(stats[:max_items_in_period])} items" \
+            "#{eta})"
     end
 
     def reset
-      state = State.new
-      @state = state
+      @state = State.new
     end
 
     def expected=(value)
       @expected = value || options[:expected]
-      @percent = @expected.present? && !@expected.zero?
-      @rate_unit = @percent ? 'min' : 'second'
-      @progress_each = nil
-
-      job.tee "Processing #{nicenumber(expected)} #{name} items"
+      job.tee "Processing #{nice_number(expected)} #{name} items"
     end
 
-    def progress_each=(value)
-      @progress_each = value || options[:progress_each] || (@percent ? 1 : 1000)
-      @progress_each_float = @progress_each.to_f
+    def progress_every=(value)
+      @progress_every = value || options[:progress_every] || 60
     end
 
-    def nicenumber(number)
+    def nice_number(number)
       number_with_delimiter(number.to_i, delimiter: ',')
     end
 
-    def niceposition(number)
-      percent? ? format('%3d%%', number) : nicenumber(number)
+    def nice_float(number)
+      number < 5 ? number.round(2) : nice_number(number)
     end
 
-    def nicefloat(number)
-      number < 5 ? number.round(2) : nicenumber(number)
+    def nice_elapsed_time(number)
+      current_time = State.current_time
+      distance_of_time_in_words(current_time, current_time + number, include_seconds: true)
     end
 
     private
@@ -68,50 +68,31 @@ module BatchManager
     attr_reader :job
 
     delegate :options, :name, :console, to: :job
-    delegate :counter, :previous_position, :previous_time, :stats, to: :state
+    delegate :counter, :previous_counter, :items_in_period, :elapsed_time, :previous_time, :stats, to: :state
 
-    def show_position(position, current_time)
+    def compute_position
+      current_time = State.current_time
       elapsed_time = current_time - previous_time
 
-      max_elapsed_time = stats[:max_elapsed_time] || 0
-      stats[:max_elapsed_time] = elapsed_time if elapsed_time > max_elapsed_time
+      return if elapsed_time < progress_every
 
-      frequency = (stats[:frequency] || 0) + 1
-      total_elapsed_time = (stats[:total_elapsed_time] || 0) + elapsed_time
-      average_elapsed_time = total_elapsed_time / frequency
-
-      console "#{identifier}\t#{niceposition(position)} " \
-            "(" \
-            "elapsed time: #{nicefloat(elapsed_time)}s, " \
-            "rate: #{niceposition(rate(elapsed_time))}/#{@rate_unit}, " \
-            "average: #{nicefloat(average_elapsed_time)}s, " \
-            "max: #{nicefloat(max_elapsed_time)}s, " \
-            "#{eta(position, elapsed_time)})"
-
-      state.previous_position = position
       state.previous_time = current_time
+      state.elapsed_time = elapsed_time
+
+      state.items_in_period = counter - previous_counter
+      state.previous_counter = counter
+
+      stats[:max_items_in_period] = items_in_period if items_in_period > (stats[:max_items_in_period] || 0)
+      stats[:frequency] = (stats[:frequency] || 0) + 1
+
+      true
     end
 
-    def current_position
-      percent? ? 100.0 * counter / expected : counter
-    end
-
-    def percent?
-      @percent
-    end
-
-    def rate(elapsed_time)
-      rate_per_second = @progress_each_float / elapsed_time
-      (percent? ? rate_per_second * 60 : rate_per_second).to_i
-    end
-
-    def eta(position, elapsed_time)
-      return unless percent?
-
-      remaining_percent = 100 - position
-      remaining_time = elapsed_time * remaining_percent
-      eta = Time.current + remaining_time.seconds
-      ", ETA #{eta.to_s(:time)}"
+    def eta
+      return unless expected && !expected.zero?
+      percent_complete = 100.0 * counter / expected
+      seconds_to_go = (expected - counter) * progress_every / items_in_period
+      ", #{nice_float(percent_complete)}% complete, ETA #{State.current_time + seconds_to_go}"
     end
 
     def identifier
--- lib/batch_manager/state.rb
+++ lib/batch_manager/state.rb
@@ -2,26 +2,36 @@ module BatchManager
   # Holds all the job reporter's state during the job
   # we can simply reset this if we need to restart
   class State
-    attr_accessor :counter, :previous_position, :previous_time, :delim, :stats
+    attr_accessor :counter, :previous_counter, :items_in_period, :elapsed_time, :previous_time, :delim, :stats
 
     def initialize
       @counter = 0
-      @previous_position = 0
-      @previous_time = Time.current
+      @previous_counter = 0
+      @elapsed_time = 0
+      @items_in_period = 0
+      @previous_time = current_time
       @delim = ''
       @stats = {}
     end
 
     def start_time
-      @start_time ||= Time.current
+      @start_time ||= current_time
     end
 
     def finish_time
-      @finish_time ||= Time.current
+      @finish_time ||= current_time
     end
 
     def elapsed_time
       @elapsed_time ||= finish_time - start_time
+    end
+
+    def current_time
+      self.class.current_time
+    end
+
+    def self.current_time
+      Time.current # zone.at Process.clock_gettime(Process::CLOCK_MONOTONIC)
     end
   end
 end
